dc52175821e8b7ca79e0e851655a6551bde1c5a7
Darkmonk08 <88379889+Darkmonk08@users.noreply.github.com>
2025-11-23 16:25:58 +0530

    Add files via upload

---
+++ memorizer.py
@@ -0,0 +1,44 @@
+from tkinter import *
+
+root=Tk()
+root.geometry("800x600")
+root.config(background="light grey")
+root.title("memorizer")
+
+def additem():
+    item=entry.get()
+    listbox.insert(END,item)
+
+def deleteitem():
+    selection=listbox.curselection()
+    print(selection)
+    if selection:
+        listbox.delete(selection[0])
+frame1=Frame(root,background="light grey")
+frame1.pack()
+
+frame2=Frame(root,background="light grey")
+frame2.pack()
+
+save=Button(frame1,text="Save",font=("Ariel",15),background="light grey",width=30)
+save.pack(side=TOP,pady=10)
+
+open=Button(frame1,text="Open",font=("Ariel",15),background="light grey",width=30)
+open.pack(side=TOP,pady=10)
+
+entry=Entry(frame1,font=("Ariel",15),width=30)
+entry.pack(side=TOP,pady=20)
+
+add=Button(frame2,text="Add",font=("Ariel",15),width=15,command=additem)
+add.pack(side=LEFT,padx=10)
+
+listbox=Listbox(frame2,font=("Ariel",15),width=30)
+listbox.pack(side=LEFT,padx=10)
+
+delete=Button(frame2,text="delete",font=("Ariel",15),width=15,command=deleteitem)
+delete.pack(side=LEFT,padx=10)
+
+
+
+root.mainloop()
+
